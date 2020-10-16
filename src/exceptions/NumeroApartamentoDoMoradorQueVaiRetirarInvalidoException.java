4c861054bc68fd2826b95ef77e1d3e95ba218142
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-10-16 16:24:28 -0300

    feat(NumeroApartamentoDoMoradorQueVaiRetirarInvalidoException.java): adiciona classe de excecao para morador invalido para retirada

---
+++ src/exceptions/NumeroApartamentoDoMoradorQueVaiRetirarInvalidoException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class NumeroApartamentoDoMoradorQueVaiRetirarInvalidoException extends Exception {
+
+  public NumeroApartamentoDoMoradorQueVaiRetirarInvalidoException(int nroApartamentoDestinatario) {
+    super("Morador inválido. Esta entrega está registrada para o morador do apartamento " + nroApartamentoDestinatario + '.');
+  }
+}
