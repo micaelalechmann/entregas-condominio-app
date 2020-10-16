f2021c978461182614659925dd1d960c2972be76
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-10-16 14:40:33 -0300

    feat(MoradorTest.java): adiciona testes ao arquivo MoradorTest.java

--- tests/classes/MoradorTest.java
+++ tests/classes/MoradorTest.java
@@ -1,4 +1,43 @@
-import static org.junit.Assert.*;
+package classes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MoradorTest {
-  
+  Morador morador;
+
+  @Before
+  public void setup() {
+    morador = new Morador("Marlon", "2244610441", 204);
+  }
+
+  @Test
+  public void deveCriarMoradorCorretamente() {
+    Assert.assertNotNull(morador);
+  }
+
+  @Test
+  public void deveRetornarONomeDoMoradorCorretamente() {
+    String nomeEsperado = "Marlon";
+    String nomeObtido = morador.getNome();
+
+    Assert.assertEquals(nomeEsperado, nomeObtido);
+  }
+
+  @Test
+  public void deveRetornarORgDoMoradorCorretamente() {
+    String rgEsperado = "2244610441";
+    String rgObtido = morador.getRg();
+
+    Assert.assertEquals(rgEsperado, rgObtido);
+  }
+
+  @Test
+  public void deveRetornarONumeroDoApartamentoDoMoradorCorretamente() {
+    int nroApartamentoEsperado = 204;
+    int nroApartamentoObtido = morador.getNroApartamento();
+
+    Assert.assertEquals(nroApartamentoEsperado, nroApartamentoObtido);
+  }
 }
