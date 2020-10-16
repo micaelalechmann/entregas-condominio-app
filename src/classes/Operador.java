fb918d3a48815bee5c7e2a80ad3968dc11e989cd
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-10-16 13:15:15 -0300

    fix(Operador.java): corrige o metodo toString para sempre retornar string em maiusculo

--- src/classes/Operador.java
+++ src/classes/Operador.java
@@ -13,7 +13,7 @@ public class Operador {
     StringBuilder sb = new StringBuilder();
 
     for (String s : iniciaisNome) {
-      sb.append(s.charAt(0));
+      sb.append(s.toUpperCase().charAt(0));
     }
 
     return sb.toString();
