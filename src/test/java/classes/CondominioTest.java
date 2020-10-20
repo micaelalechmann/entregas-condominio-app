6c93acc5923aaac5f069c32b5cbccf1db4303133
AndrewsSouza <andrews.souza@outlook.com.br>
2020-10-19 23:29:35 -0300

    test: criando testes para a busca e a listagem de entregas por descricao

---
+++ src/test/java/classes/CondominioTest.java
@@ -0,0 +1,120 @@
+package classes;
+
+import main.java.classes.Condominio;
+import main.java.classes.Entrega;
+import main.java.classes.Operador;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CondominioTest {
+    Condominio condominio;
+    Operador operador;
+
+    @Before
+    public void setup(){
+        condominio = new Condominio();
+        operador = new Operador("Operador Teste");
+    }
+
+    @Test
+    public void listaEntregasPelaDescricao_testaListagem() {
+        // arrange
+        String textoContido = "Teste";
+        String descricaoEsperada = "Descricao " + textoContido;
+        String descricaoNaoEsperada = "Pacote";
+        Entrega entrega1 = new Entrega(1, descricaoEsperada, operador, 1);
+        Entrega entrega2 = new Entrega(2, textoContido, operador, 2);
+        Entrega entrega3 = new Entrega(3, descricaoNaoEsperada, operador, 3);
+        List<Entrega> entregas = Arrays.asList(entrega1, entrega2, entrega3);
+
+        condominio.setEntregas(entregas);
+
+        // act
+        condominio.listaEntregasPelaDescricao(textoContido);
+    }
+
+    @Test
+    public void buscaEntregasPelaDescricao_retornaUmaDeDuasEntregas() {
+        // arrange
+        String descricaoEsperada = "Descricao Teste";
+        String descricaoNaoEsperada = "Pacote";
+        Entrega entrega1 = new Entrega(1, descricaoEsperada, operador, 1);
+        Entrega entrega2 = new Entrega(2, descricaoNaoEsperada, operador, 2);
+        List<Entrega> entregas = Arrays.asList(entrega1, entrega2);
+        condominio.setEntregas(entregas);
+
+        // act
+        List<Entrega> resultado = condominio.buscaEntregasPelaDescricao(descricaoEsperada);
+
+        // assert
+        assertEquals(1, resultado.size());
+        assertEquals(descricaoEsperada, resultado.get(0).getDescricao());
+    }
+
+    @Test
+    public void buscaEntregasPelaDescricao_retornaUmaDeDuasEntregas_contendoDescricao() {
+        // arrange
+        String textoContido = "Teste";
+        String descricaoEsperada = "Descricao " + textoContido;
+        String descricaoNaoEsperada = "Pacote";
+        Entrega entrega1 = new Entrega(1, descricaoEsperada, operador, 1);
+        Entrega entrega2 = new Entrega(2, textoContido, operador, 2);
+        Entrega entrega3 = new Entrega(3, descricaoNaoEsperada, operador, 3);
+        List<Entrega> entregas = Arrays.asList(entrega1, entrega2, entrega3);
+
+        condominio.setEntregas(entregas);
+
+        // act
+        List<Entrega> resultado = condominio.buscaEntregasPelaDescricao(textoContido);
+
+        // assert
+        assertEquals(2, resultado.size());
+        assertEquals(descricaoEsperada, resultado.get(0).getDescricao());
+        assertTrue(resultado.get(1).getDescricao().contains(textoContido));
+    }
+
+    @Test
+    public void buscaEntregasPelaDescricao_retornaUmaDeDuasEntregas_contendoDescricao_ignoreCase() {
+        // arrange
+        String textoContido = "teste";
+        String descricaoEsperada = "Descricao " + textoContido.toUpperCase();
+        String descricaoNaoEsperada = "Pacote";
+        Entrega entrega1 = new Entrega(1, descricaoEsperada, operador, 1);
+        Entrega entrega2 = new Entrega(2, textoContido, operador, 2);
+        Entrega entrega3 = new Entrega(3, descricaoNaoEsperada, operador, 3);
+        List<Entrega> entregas = Arrays.asList(entrega1, entrega2, entrega3);
+
+        condominio.setEntregas(entregas);
+
+        // act
+        List<Entrega> resultado = condominio.buscaEntregasPelaDescricao(textoContido);
+
+        // assert
+        assertEquals(2, resultado.size());
+        assertEquals(descricaoEsperada, resultado.get(0).getDescricao());
+        assertTrue(resultado.get(1).getDescricao().contains(textoContido));
+    }
+
+    @Test
+    public void buscaEntregasPelaDescricao_retornaZeroEntregas() {
+        // arrange
+        String descricaoEsperada = "Descricao Teste";
+        String descricaoNaoEsperada = "Pacote";
+        Entrega entrega1 = new Entrega(1, descricaoNaoEsperada, operador, 2);
+
+        List<Entrega> entregas = Collections.singletonList(entrega1);
+        condominio.setEntregas(entregas);
+
+        // act
+        List<Entrega> resultado = condominio.buscaEntregasPelaDescricao(descricaoEsperada);
+
+        // assert
+        assertEquals(0, resultado.size());
+    }
+}
