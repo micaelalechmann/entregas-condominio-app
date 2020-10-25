2eb8941161379fb21209a1d5b20f2464305bbe6b
Matheus Pozzer <matheuspozzermoraes@gmail.com>
2020-10-25 00:51:52 -0300

    feat: Criacao de gerarRelatorio e RegistrarRetiradaEntrega

--- src/main/java/classes/App.java
+++ src/main/java/classes/App.java
@@ -1,7 +1,13 @@
 package classes;
 
+import java.time.LocalDateTime;
+
 public class App {
     public static void main(String[] args) {
+//        Menu menu2 = new Menu(new Condominio());
+//        LocalDateTime dataInicial = LocalDateTime.of(2020,10,22,20,30);
+//        LocalDateTime dataFinal = LocalDateTime.of(2020,10,30,20,30);
+//        menu2.gerarRelatorio(dataInicial,dataFinal);
         Menu menu = new Menu(new Condominio());
         menu.run();
     }
--- src/main/java/classes/Menu.java
+++ src/main/java/classes/Menu.java
@@ -1,12 +1,18 @@
 package classes;
 
+import exceptions.EntregaJaFoiRetiradaException;
+import exceptions.NumeroApartamentoDoMoradorQueVaiRetirarInvalidoException;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Menu {
     private Condominio condominio;
     private Operador operadorAtual;
     private Scanner scanner;
+    private List<Entrega> entregasRetiradas = new ArrayList<>();
 
     public Menu(Condominio condominio){
         this.condominio = condominio;
@@ -167,7 +173,8 @@ public class Menu {
     public void menuOperador() {
         System.out.println("1 - Cadastrar operador");
         System.out.println("2 - Selecionar operador");
-        System.out.println("3 - Voltar ");
+        System.out.println("3 - Registrar retirada da entrega");
+        System.out.println("5 - Voltar ");
 
         this.scanner.reset();
         int numOpcao = recebeNumero();
@@ -182,6 +189,43 @@ public class Menu {
                 this.menuOperador();
                 break;
             case 3:
+                try {
+                    this.RegistrarRetiradaEntrega();
+                } catch (EntregaJaFoiRetiradaException | NumeroApartamentoDoMoradorQueVaiRetirarInvalidoException e) {
+                    e.printStackTrace();
+                }
+                this.menuOperador();
+                break;
+            case 4:
+//                System.out.println("Insira a data inicial do relatorio");
+//                System.out.println("Primeiro insira o ano:");
+//                int opcao = recebeNumero();
+//                System.out.println("O mes:");
+//                int opcao2 = recebeNumero();
+//                System.out.println("O dia:");
+//                int opcao3 = recebeNumero();
+//                System.out.println("A hora:");
+//                int opcao4 = recebeNumero();
+//                System.out.println("Os minutos");
+//                int opcao5 = recebeNumero();
+//                LocalDateTime dataInicial = LocalDateTime.of(opcao,opcao2,opcao3,opcao4,opcao5);
+//                System.out.println("Agora Insira a data final do relatorio");
+//                System.out.println("Primeiro insira o ano:");
+//                int opcaoo = recebeNumero();
+//                System.out.println("O mes:");
+//                int opcaoo2 = recebeNumero();
+//                System.out.println("O dia:");
+//                int opcaoo3 = recebeNumero();
+//                System.out.println("A hora:");
+//                int opcaoo4 = recebeNumero(); LocalDateTime dataFinal = LocalDateTime.of(opcao,opcao2,opcao3,opcao4,opcao5);
+//                System.out.println("Os minutos");
+//                int opcaoo5 = recebeNumero();
+                LocalDateTime dataInicial = LocalDateTime.of(2020,10,22,20,30);
+                LocalDateTime dataFinal = LocalDateTime.of(2020,10,30,20,30);
+                this.gerarRelatorio(dataInicial,dataFinal);
+                this.menuOperador();
+                break;
+            case 5:
                 break;
             default:
                 System.out.println("Opção inexistente");
@@ -213,5 +257,79 @@ public class Menu {
 
         operadorAtual = condominio.getOperadores().get(indiceOperador - 1);
         this.scanner.reset();
+    }
+    public void RegistrarRetiradaEntrega()
+            throws EntregaJaFoiRetiradaException, NumeroApartamentoDoMoradorQueVaiRetirarInvalidoException {
+        boolean testandoNumeroApartamento = false;
+        boolean testandoId= false;
+        List<Entrega> entregasNaoRetiradas = this.condominio.buscaEntregasNaoRetiradas();
+        listarEntregasNaoRetiradas();
+        if (this.condominio.buscaEntregasNaoRetiradas().size() > 0) {
+            System.out.println("Selecione o id da entrega que deseja");
+            int id = recebeNumero();
+            for (Entrega entregasNaoRetirada : entregasNaoRetiradas) {
+                if (id == entregasNaoRetirada.getIdEntrega()) {
+                    testandoId = true;
+                }
+            }
+            if (testandoId) {
+                List<Morador> moradorQueVaiRetirar = this.condominio.getMoradores();
+                System.out.println(moradorQueVaiRetirar.toString().replace('[', ' ').replace(']', ' '));
+                System.out.println("Coloque o numero do apartamento do morador que vai retirar a entrega");
+                int numeroApartamento = recebeNumero();
+                for (Morador morador : moradorQueVaiRetirar) {
+                    if (numeroApartamento == morador.getNumeroApartamento()) {
+                        testandoNumeroApartamento = true;
+                        for (Entrega entregasNaoRetirada : entregasNaoRetiradas) {
+                            if (id == entregasNaoRetirada.getIdEntrega()) {
+                                entregasNaoRetirada.retirarEntrega(morador);
+                                entregasRetiradas.add(entregasNaoRetirada);
+                            }
+                        }
+                    }
+                }
+            }
+            if (!testandoId) {
+                System.out.println("Erro: Id nao localizado");
+            }
+            else if (!testandoNumeroApartamento) {
+                System.out.println("Erro: Numero de apartamento nao localizado");
+            }
+        }
+    }
+    public void gerarRelatorio(LocalDateTime dataInicial,LocalDateTime dataFinal) {
+        List<Entrega> entregasNaoRetiradas = this.condominio.buscaEntregasNaoRetiradas();
+        entregasNaoRetiradas.addAll(entregasRetiradas);
+        List<Entrega> entregasEntreAsDatas = new ArrayList<>();
+        for (Entrega entregasNaoRetirada : entregasNaoRetiradas) {
+            boolean isBefore = entregasNaoRetirada.getDataRegistro().isBefore(dataFinal);
+            boolean isAfter = entregasNaoRetirada.getDataRegistro().isAfter(dataInicial);
+            if (isBefore && isAfter) {
+                entregasEntreAsDatas.add(entregasNaoRetirada);
+            }
+        }
+
+        String leftAlignFormat = "| %-8d | %-19s |";
+        String leftAlignFormat2 =" %-50s | %-5d |";
+        String leftAlignFormat3 =" %-8s | %-19s |";
+        String leftAlignFormat4 ="%-20s |%n";
+        System.out.format("+----------+---------------------+----------------------+-----------------------------+-------+----------+---------------------+---------------------+%n");
+        System.out.format("| Entrega  |      Data/hora      |                    Descricao                       |  Apto | Operador |      Retirada       |        Morador      |%n");
+        System.out.format("+----------+---------------------+----------------------+-----------------------------+-------+----------+---------------------+---------------------+%n");
+        for (Entrega entregasEntreAsData : entregasEntreAsDatas) {
+            if (entregasEntreAsData.getDataRetirada() == null && entregasEntreAsData.getMoradorQueRetirou() == null) {
+                System.out.format(leftAlignFormat, entregasEntreAsData.getIdEntrega(), entregasEntreAsData.getDataRegistro().toString().substring(0, 19).replace('T', ' '));
+                System.out.format(leftAlignFormat2, entregasEntreAsData.getDescricao(), entregasEntreAsData.getNumeroApartamentoDestinatario());
+                System.out.format(leftAlignFormat3, entregasEntreAsData.getOperadorResponsavel(), "NR");
+                System.out.format(leftAlignFormat4, "NR");
+            } else {
+                System.out.format(leftAlignFormat, entregasEntreAsData.getIdEntrega(), entregasEntreAsData.getDataRegistro().toString().substring(0, 19).replace('T', ' '));
+                System.out.format(leftAlignFormat2, entregasEntreAsData.getDescricao(), entregasEntreAsData.getNumeroApartamentoDestinatario());
+                System.out.format(leftAlignFormat3, entregasEntreAsData.getOperadorResponsavel(), entregasEntreAsData.getDataRetirada().toString().substring(0, 19).replace('T', ' '));
+                System.out.format(leftAlignFormat4, entregasEntreAsData.getMoradorQueRetirou().getNome());
+            }
+        }
+        System.out.format("+----------+---------------------+----------------------+-----------------------------+-------+----------+---------------------+---------------------+%n");
+        System.out.println("NR = Nao retirado \n");
     }
 }
